6ae8a966e77838849a4d5f2471e15c49a315aec7
John Lockwood <johnlockwood@me.com>
2023-06-05 21:29:52 +0100

    fix: Fixed path to Unity Tennis.exe

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import torch
 from maddpg import MaddpgAgent
 
 if __name__ in "__main__":
-    env = UnityEnvironment(file_name="C:/Users/johnb/repos/deep-reinforcement-learning/p3_collab-compet/Tennis_Windows_x86_64/Tennis.exe")
-
+    root_dir = "C:/Users/johnb/repos/Udacity/deep-reinforcement-learning/p3_collab-compet/Tennis_Windows_x86_64"
+    env = UnityEnvironment(file_name=root_dir+"/Tennis.exe")
     log_path = os.getcwd()+"/log"
     model_dir= os.getcwd()+"/model_dir"
     
@@ -50,30 +50,31 @@ if __name__ in "__main__":
     maddpg_agent = MaddpgAgent(state_size, action_size, discount_factor=0.99, tau = 1e-2,
                         num_agents=num_agents, buffer_size=int(1e5), batch_size=128, seed=44)
     
-    # for episode in range(int(1e3)):                                         # play game for 5 episodes
-    #     env_info = env.reset(train_mode=True)[brain_name]     # reset the environment    
-    #     obs = env_info.vector_observations                  # get the current state (for each agent)
-    #     score = np.zeros(num_agents)                          # initialize the score (for each agent)
-    #     for step in range(max_step):
-    #         actions = np.random.randn(num_agents, action_size) # select an action (for each agent)
-    #         # actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
-    #         # print(actions)
-    #         env_info = env.step(actions)[brain_name]           # send all actions to tne environment
-    #         next_obs = env_info.vector_observations         # get next state (for each agent)
-    #         rewards = env_info.rewards                         # get reward (for each agent)
-    #         dones = env_info.local_done                        # see if episode finished
-    #         score += rewards                                   # update the score (for each agent)
+    for episode in range(int(1e3)):                                         # play game for 5 episodes
+        env_info = env.reset(train_mode=True)[brain_name]     # reset the environment    
+        obs = env_info.vector_observations                  # get the current state (for each agent)
+        score = np.zeros(num_agents)                          # initialize the score (for each agent)
+        for step in range(max_step):
+            actions = np.random.randn(num_agents, action_size) # select an action (for each agent)
+            actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
+            # print(actions)
+            env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+            next_obs = env_info.vector_observations         # get next state (for each agent)
+            rewards = env_info.rewards                         # get reward (for each agent)
+            dones = env_info.local_done                        # see if episode finished
+            score += rewards                                   # update the score (for each agent)
            
-    #         if np.any(dones):                                  # exit loop if episode finished
-    #             break
+            if np.any(dones):                                  # exit loop if episode finished
+                break
 
-    #         agent.add(obs, actions, rewards, next_obs, dones)
+            maddpg_agent.add(obs, actions, rewards, next_obs, dones)
 
-    #         obs = next_obs                              # roll over states to next time step
-    # print("Memory Full...")
+            obs = next_obs                              # roll over states to next time step
+    print("Memory Full...")
 
     total_steps = 0
     scores = []
+    best_score = 0
     for episode in range(num_episodes):                                         # play game for 5 episodes
         env_info = env.reset(train_mode=True)[brain_name]     # reset the environment    
         obs = env_info.vector_observations                  # get the current state (for each agent)
@@ -91,13 +92,6 @@ if __name__ in "__main__":
             maddpg_agent.update(obs, actions, rewards, next_obs, dones)
             score += rewards                                   # update the score (for each agent)
            
-            
-
-            # agent.add(obs, actions, rewards, next_obs, dones)
-           
-            # maddpg_agent.update(obs, actions, rewards, next_obs, dones)
-            # if total_steps % steps_per_update == 0:
-            # agent.update() 
 
             obs = next_obs                              # roll over states to next time step
 
@@ -112,19 +106,22 @@ if __name__ in "__main__":
         if (episode % 100 == 0 or episode == num_episodes-1) and episode != 0:
             # print(actions)
             # print(obs)
-            print('Ep: {} Average score: {}'.format(episode, np.mean(scores[-100:])))
+            mean_scores = np.mean(scores[-100:])
+            print('Ep: {} Average score: {}'.format(episode, mean_scores))
 
-                    #saving model
-            save_dict_list =[] 
-            for i in range(2):
+            if mean_scores > best_score:
+                #saving model
+                save_dict_list =[] 
+                for i in range(2):
 
-                save_dict = {'actor_params' : maddpg_agent.agents[i].actor_local.state_dict(),
-                            'actor_optim_params': maddpg_agent.agents[i].actor_optimizer.state_dict(),
-                            'critic_params' : maddpg_agent.agents[i].critic_local.state_dict(),
-                            'critic_optim_params' : maddpg_agent.agents[i].critic_optimizer.state_dict()}
-                save_dict_list.append(save_dict)
+                    save_dict = {'actor_params' : maddpg_agent.agents[i].actor_local.state_dict(),
+                                'actor_optim_params': maddpg_agent.agents[i].actor_optimizer.state_dict(),
+                                'critic_params' : maddpg_agent.agents[i].critic_local.state_dict(),
+                                'critic_optim_params' : maddpg_agent.agents[i].critic_optimizer.state_dict()}
+                    save_dict_list.append(save_dict)
 
-                torch.save(save_dict_list, 
-                        os.path.join(model_dir, 'episode-{}.pt'.format(episode)))
+                    torch.save(save_dict_list, 
+                            os.path.join(model_dir, 'episode-{}.pt'.format(episode)))
+                best_score = mean_scores
 
     env.close()
